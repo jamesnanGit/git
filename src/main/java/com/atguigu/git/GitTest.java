5e37e1be89d236f31ca39692d25cc5624bb2e73e
jamesnanGit <jamesnangit@163.com>
2022-03-28 11:33:19 +0800

    hotfix fix1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("my first git test");
         System.out.println("second git test");
         System.out.println("hot fix test");
+        System.out.println("hot fix1");
     }
 }
