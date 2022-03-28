8043684cf404b6b76c68dbed0ecd2387a76839d4
jamesnanGit <jamesnangit@163.com>
2022-03-28 11:05:04 +0800

    first git commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author JamesLin
+ * @className git
+ * @description TODO
+ * @date 2022/3/28 11:02
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("my first git test");
+    }
+}
