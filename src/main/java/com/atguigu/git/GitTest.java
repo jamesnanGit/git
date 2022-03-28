2a6ab262266d83e6dccf4dcca5c325d5ad8c4644
jamesnanGit <jamesnangit@163.com>
2022-03-28 11:28:17 +0800

    hotfix first commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("my first git test");
         System.out.println("second git test");
+        System.out.println("hot fix test");
     }
 }
